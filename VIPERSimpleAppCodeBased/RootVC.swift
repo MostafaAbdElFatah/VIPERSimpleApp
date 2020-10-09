696e55cc4613f02b6489c5b5d983b8c474b1a6fc
MostafaAbdElFatah <m.abdelfatah@gmail.com>
2020-10-09 19:21:21 +0200

    Initial Commit

---
+++ VIPERSimpleAppCodeBased/RootVC.swift
@@ -0,0 +1,20 @@
+//
+//  RootVC.swift
+//  VIPERSimpleAppCodeBased
+//
+//  Created by Mostafa Abd ElFatah on 10/9/20.
+//  Copyright © 2020 MostafaAbdElFatah. All rights reserved.
+//
+
+import UIKit
+
+class RootVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
